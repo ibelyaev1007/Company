b0ead7d5080bcf9c097aa91452bf206abecb41d3
Ivan Belyaev <Ivan.Belyaev@emerson.com>
2020-06-02 09:22:21 +0500

    Добалены компоненты Базовая настройка сервисов

--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,24 +17,32 @@ namespace MyCompany
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Поддержка контроллеров и представлений (MVC) и выставляем совместимость с asp net core 3
+            services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //!!! порядок регистрации middleware очень важен
+
+            //в процессе разработки нам важно видеть какие именно ошибки
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //подключаем систему маршрутизации
             app.UseRouting();
 
+            //подключаем поддержку статичных файлов в приложении (css, js и т.д.)
+            app.UseStaticFiles();
+
+            //регистриуруем нужные нам маршруты (ендпоинты)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
